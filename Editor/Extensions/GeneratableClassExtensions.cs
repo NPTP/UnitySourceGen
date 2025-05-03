4e52b634000e7f369a78cfbe307ae421bd14da9a
Nick Perrin <nick.perrin@hotmail.com>
2025-05-02 23:18:05 -0400

    Name safety checks, remove extraneous license

--- Editor/Extensions/GeneratableClassExtensions.cs
+++ Editor/Extensions/GeneratableClassExtensions.cs
@@ -1,5 +1,6 @@
 using NPTP.UnitySourceGen.Editor.Enums;
 using NPTP.UnitySourceGen.Editor.Generatable;
+using UnityEngine;
 
 namespace NPTP.UnitySourceGen.Editor.Extensions
 {
@@ -31,56 +32,76 @@ namespace NPTP.UnitySourceGen.Editor.Extensions
 
         public static GeneratableTypeDefinition WithField<T>(this GeneratableTypeDefinition gen, string fieldName, AccessModifier accessModifier)
         {
+            if (!CheckValidName(fieldName)) return gen;
             gen.AddField(new GeneratableField<T>(fieldName, accessModifier, isStatic: false));
             return gen;
         }
 
         public static GeneratableTypeDefinition WithField<T>(this GeneratableTypeDefinition gen, string fieldName, AccessModifier accessModifier, T initialValue)
         {
+            if (!CheckValidName(fieldName)) return gen;
             gen.AddField(new GeneratableField<T>(fieldName, accessModifier, isStatic: false, initialValue));
             return gen;
         }
         
         public static GeneratableTypeDefinition WithStaticField<T>(this GeneratableTypeDefinition gen, string fieldName, AccessModifier accessModifier)
         {
+            if (!CheckValidName(fieldName)) return gen;
             gen.AddField(new GeneratableField<T>(fieldName, accessModifier, isStatic: true));
             return gen;
         }
 
         public static GeneratableTypeDefinition WithStaticField<T>(this GeneratableTypeDefinition gen, string fieldName, AccessModifier accessModifier, T initialValue)
         {
+            if (!CheckValidName(fieldName)) return gen;
             gen.AddField(new GeneratableField<T>(fieldName, accessModifier, isStatic: true, initialValue));
             return gen;
         }
         
         public static GeneratableTypeDefinition WithConstField<T>(this GeneratableTypeDefinition gen, string fieldName, AccessModifier accessModifier, T initialValue)
         {
+            if (!CheckValidName(fieldName)) return gen;
             gen.AddField(new GeneratableConstField<T>(fieldName, accessModifier, initialValue));
             return gen;
         }
 
         public static GeneratableTypeDefinition WithProperty(this GeneratableTypeDefinition gen, string propertyName, AccessModifier getModifier, AccessModifier setModifier, bool isStatic)
         {
+            if (!CheckValidName(propertyName)) return gen;
             gen.AddProperty(new GeneratableProperty(propertyName, getModifier, setModifier, isStatic));
             return gen;
         }
 
         public static GeneratableTypeDefinition WithStaticMethod<T>(this GeneratableTypeDefinition gen, string methodName, AccessModifier accessModifier, params string[] body)
         {
+            if (!CheckValidName(methodName)) return gen;
             gen.AddMethod(new GeneratableMethod<T>(methodName, accessModifier, InheritanceModifier.None, isStatic: true, body));
             return gen;
         }
         
         public static GeneratableTypeDefinition WithMethod<T>(this GeneratableTypeDefinition gen, string methodName, AccessModifier accessModifier, params string[] body)
         {
+            if (!CheckValidName(methodName)) return gen;
             gen.AddMethod(new GeneratableMethod<T>(methodName, accessModifier, InheritanceModifier.None, isStatic: false, body));
             return gen;
         }
         
         public static GeneratableTypeDefinition WithMethod<T>(this GeneratableTypeDefinition gen, string methodName, AccessModifier accessModifier, InheritanceModifier inheritanceModifier, params string[] body)
         {
+            if (!CheckValidName(methodName)) return gen;
             gen.AddMethod(new GeneratableMethod<T>(methodName, accessModifier, inheritanceModifier, isStatic: false, body));
             return gen;
+        }
+
+        private static bool CheckValidName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                Debug.LogWarning("Tried to add a null- or empty-named element to a generatable type definition.");
+                return false;
+            }
+            
+            return true;
         }
         
     }
